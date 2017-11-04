03853802e7ad7c77408842e2ea46f8d05723be96
Yuji Suzuki <y0suzuki0123@gmail.com>
2017-11-04 10:18:43 +0900

    Add create-component command

---
+++ scripts/create-component/templates/Component/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import styles from './index.scss';
+
+export default function {{componentName}}() {
+  return <div className={styles.foo}></div>;
+}
---
+++ scripts/create-component/templates/Component/index.story.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import {{componentName}} from '.';
+
+storiesOf('{{componentName}}', module)
+  .add('foo', () => <{{componentName}} name="foo" />);
