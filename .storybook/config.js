0d12dcbb427e3ef4467f53290f0cc98677bf8e32
Yuji Suzuki <y0suzuki0123@gmail.com>
2017-11-09 10:06:43 +0900

    Move stories into stories/

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,6 +1,6 @@
 import { configure } from '@storybook/react';
 
-const req = require.context('../src', true, /\.story\.js$/);
+const req = require.context('../stories', true, /\.story\.js$/);
 
 function loadStories() {
   req.keys().forEach((filename) => req(filename))
--- stories/HelloWorld/index.story.js
+++ stories/HelloWorld/index.story.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import HelloWorld from '.';
+import HelloWorld from '../../src/HelloWorld';
 
 storiesOf('HelloWorld', module)
   .add('world', () => <HelloWorld name="world" />)
