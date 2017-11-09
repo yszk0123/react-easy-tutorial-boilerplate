a9bc88146452d8930362d685b52b49941a130021
Yuji Suzuki <y0suzuki0123@gmail.com>
2017-11-09 11:54:48 +0900

    Move HelloWorld

--- stories/components/HelloWorld/index.story.js
+++ stories/components/HelloWorld/index.story.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import HelloWorld from '../../src/HelloWorld';
+import HelloWorld from '../../../src/components/HelloWorld';
 
 storiesOf('HelloWorld', module)
   .add('world', () => <HelloWorld name="world" />)
